520b93dbd3d628273d3a34dec1355209ea1e1d37
Pietraceci <pietracecily@gmail.com>
2022-07-03 23:27:42 -0400

    criando repositório

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css"
+
+function App(){
+    const valores = [
+        "linux", "docker", "server", "rails", "douane", "my-developments","command-line", "ruby", "c-2", "linux-on-mac", "maintenance", "security", "gnome", "kubernetes", "mes-developpements", "ubuntu", "python", "vcs", "capybara", "contribution", "git", "apt","bazaar", "chef", "cucumber", "debian", "game", "howto", "packaging", "testing", "boost", "debug","devise", "elixir", "gtk", "homebrew", "information", "ssh", "tuxtremsplit", "xpath"
+    ];
+
+    const lista = valores.map(
+        (c)=> 
+        <p className='box'>{c}</p>
+      );
+
+	return(
+        <div className='content'>
+            {lista}
+        </div>
+
+    );
+
+}
+export default App;
