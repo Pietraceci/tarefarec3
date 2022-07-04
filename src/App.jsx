bcdb59ee515575480d6b21117fe3fc38b3e0b68d
Pietraceci <pietracecily@gmail.com>
2022-07-04 00:05:02 -0400

    atualizando dados

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,20 @@ import "./App.css"
 import News from './News'
 
 function App(){
-    const valores = [
-        "linux", "docker", "server", "rails", "douane", "my-developments","command-line", "ruby", "c-2", "linux-on-mac", "maintenance", "security", "gnome", "kubernetes", "mes-developpements", "ubuntu", "python", "vcs", "capybara", "contribution", "git", "apt","bazaar", "chef", "cucumber", "debian", "game", "howto", "packaging", "testing", "boost", "debug","devise", "elixir", "gtk", "homebrew", "information", "ssh", "tuxtremsplit", "xpath"
-    ];
+    return( 
+       <div>
+        <p className='text'>
+           Mais lidas
+        </p>  
+        <News />
 
-    const lista = valores.map(
-        (c)=> 
-        <p className='box'>{c}</p>
-      );
-
-	return(
-        <div className='content'>
-            {lista}
+        <div className='text'>
+            Mais lidas
         </div>
-
-    );
-
+        <News/>  
+   
+   
+       </div>
+   )
 }
 export default App;
